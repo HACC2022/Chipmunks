29c8ef9c468969bf304a4f1885deca2a0403d0c5
Ethan Chee <ethancheez@gmail.com>
2022-10-24 23:32:03 -1000

    redesign classes page

--- app/imports/ui/pages/Classes.jsx
+++ app/imports/ui/pages/Classes.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { Container, Button, Table } from 'react-bootstrap';
+import { _ } from 'meteor/underscore';
+import { Container, Button, Table, ListGroup, InputGroup, Form, Accordion, Card, Tabs, Tab } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Roles } from 'meteor/alanning:roles';
 import { Meteor } from 'meteor/meteor';
+import { Search } from 'react-bootstrap-icons';
 import { PAGE_IDS } from '../utilities/PageIDs';
 import { Sessions } from '../../api/session/SessionCollection';
 import { Lessons } from '../../api/lesson/LessonCollection';
@@ -11,12 +13,12 @@ import CreateSessionModal from '../components/session/CreateSessionModal';
 import CreateLessonModal from '../components/lesson/CreateLessonModal';
 import LoadingSpinner from '../components/LoadingSpinner';
 import LessonItem from '../components/LessonItem';
+import ClassesEventItem from '../components/classes/ClassesEventItem';
+import ClassesCourseItem from '../components/classes/ClassesCourseItem';
 
-/** Render a Not Found page if the user enters a URL that doesn't match any route. */
 const Classes = () => {
   const [showCreateSession, setShowCreateSession] = useState(false);
   const [showCreateLesson, setShowCreateLesson] = useState(false);
-  const [lessonsShown, setLessonsShown] = useState([]);
 
   const { ready, sessions, lessons } = useTracker(() => {
     const subscription1 = Sessions.subscribeSession();
@@ -33,20 +35,9 @@ const Classes = () => {
     };
   }, []);
 
-  const toggleShown = (sessionOwner) => {
-    const shownState = lessonsShown.slice();
-    const index = shownState.indexOf(sessionOwner);
-    if (index >= 0) {
-      shownState.splice(index, 1);
-      setLessonsShown(shownState);
-    } else {
-      shownState.push(sessionOwner);
-      setLessonsShown(shownState);
-    }
-  };
-
   return (ready ? (
     <Container id={PAGE_IDS.CLASSES_PAGE} className="py-3">
+      <h1>Classes</h1>
       { Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN]) ? (
         <div>
           <Button variant="outline-primary" type="button" onClick={() => setShowCreateSession(true)}>Create Course / Event</Button>
@@ -56,34 +47,41 @@ const Classes = () => {
           <CreateLessonModal lessonModal={{ show: showCreateLesson, setShow: setShowCreateLesson }} sessionModal={{ show: showCreateSession, setShow: setShowCreateSession }} />
         </div>
       ) : ''}
-      <Table striped hover style={{ marginTop: '25px' }}>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Summary</th>
-            <th>Type</th>
-            <th>Difficulty</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            sessions.map((session) => ([
-              <tr onClick={() => toggleShown(session.owner)} key={session._id}>
-                <td>{session.title}</td>
-                <td>{session.summary}</td>
-                <td>{session.type}</td>
-                <td>{session.difficulty}</td>
-              </tr>,
-              <tr>
-                {lessonsShown.includes(session.owner) && (
-                  lessons.map((lesson) => <td colSpan="4"><LessonItem lesson={lesson} /></td>)
-                )}
-              </tr>,
-            ]))
-          }
-        </tbody>
-      </Table>
 
+      <Card>
+        <ListGroup variant="flush">
+          <ListGroup.Item className="p-4">
+            <InputGroup>
+              <InputGroup.Text style={{ border: 'none' }}><Search /></InputGroup.Text>
+              <Form.Control
+                id="account-list-search"
+                type="search"
+                placeholder="Search"
+                style={{ backgroundColor: '#e9ecef', border: 'none' }}
+                autoComplete="off"
+              />
+              <Button variant="dark" type="button" style={{ marginLeft: '2em' }}>Search Classes</Button>
+            </InputGroup>
+          </ListGroup.Item>
+          <ListGroup.Item className="p-4">
+            <Tabs
+              defaultActiveKey="courses"
+              className="mb-3"
+              justify
+            >
+              <Tab eventKey="courses" title="Courses">
+                <Accordion>
+                  {_.where(sessions, { type: 'Course' }).map((course, index) => <ClassesCourseItem key={index} eventKey={index} session={course} lessons={_.where(lessons, { sessionID: course._id })} />)}
+                </Accordion>
+              </Tab>
+              <Tab eventKey="events" title="Events">
+                {_.where(sessions, { type: 'Event' }).map((event, index) => <ClassesEventItem key={index} eventKey={index} session={event} />)}
+              </Tab>
+            </Tabs>
+            <Accordion />
+          </ListGroup.Item>
+        </ListGroup>
+      </Card>
     </Container>
   ) : <LoadingSpinner message="Loading Classes" />);
 };
